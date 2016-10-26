0dd7e1405101b48cd98a0c5aca074147afd9530f
K. Adam White <kadamwhite@users.noreply.github.com>
2016-10-26 14:01:35 -0400

    Permit topics to be selected and button-ify the bar charts

--- src/components/TopicBarChart.js
+++ src/components/TopicBarChart.js
@@ -8,12 +8,20 @@ import './TopicBarChart.scss';
 class TopicBarChart extends PureComponent {
   static propTypes = {
     topics: PropTypes.array,
-    selected: PropTypes.object,
+    selectedEmoji: PropTypes.object,
+    selectedTopic: PropTypes.object,
+    onSelect: PropTypes.func,
     width: PropTypes.number
   }
 
   render() {
-    const { topics, selected, width } = this.props;
+    const {
+      onSelect,
+      selectedEmoji,
+      selectedTopic,
+      topics,
+      width
+    } = this.props;
 
     const containerStyles = width ? {
       maxWidth: `${width}px`
@@ -25,7 +33,7 @@ class TopicBarChart extends PureComponent {
 
     const responseCount = d3.sum(topics, d => d.count);
 
-    const emojiImageElement = selected && inlineEmoji(selected.answer);
+    const emojiImageElement = selectedEmoji && inlineEmoji(selectedEmoji.answer);
 
     /* eslint-disable no-confusing-arrow */
     const pluralizePeople = count => (count === 1) ? 'person' : 'people';
@@ -39,16 +47,21 @@ class TopicBarChart extends PureComponent {
         </p>
         <div className="bar-group-container">
           <p className="note">
-            {selected && `${responseCount} ${pluralizePeople(responseCount)} said `}
-            {selected && emojiImageElement}
-            {!selected && `${responseCount} total responses`}
+            {selectedEmoji && `${responseCount} ${pluralizePeople(responseCount)} said `}
+            {selectedEmoji && emojiImageElement}
+            {!selectedEmoji && `${responseCount} total responses`}
           </p>
           {topics.map((topic) => {
             const percentage = topic.count ?
               d3.format('.0%')(topic.count / responseCount) :
               0;
             return (
-              <div key={topic.answer} className="bar-group">
+              <button
+                aria-pressed={selectedTopic && (topic.id === selectedTopic.id)}
+                onClick={() => onSelect(topic.id)}
+                key={topic.answer}
+                className="bar-group"
+              >
                 <div className="topic-detail-container">
                   <div className="topic-name">{topic.answer}</div>
                   <div className="percentage">{percentage}</div>
@@ -56,7 +69,7 @@ class TopicBarChart extends PureComponent {
                 <div className="topic-bar-container">
                   <div className="topic-bar" style={{ width: percentage }} />
                 </div>
-              </div>
+              </button>
             );
           })}
         </div>
