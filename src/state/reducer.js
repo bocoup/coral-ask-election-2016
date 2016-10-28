9d775b2ad6c250bc6e8601b297db59ae75e4702b
K. Adam White <kadamwhite@users.noreply.github.com>
2016-10-28 00:29:33 -0400

    Update API and reducers to match final JSON export syntax (WIP)
    WIP because the app as-is does not properly load; the groundwork in the
    questions reducer sets up the next PR, which will be merged into this
    before this is merged into master.
    
    - The questions and aggregations files are combined into the form digest
    - There is an "all" key in the aggregations, which provides a quick way
      to get the total count but can otherwise be ignored
    - Given the new combined syntax we can do smarter data pre-processing
      when populating the questions store to dramatically simplify the
      aggregation querying/question locating process later on

--- src/state/reducer.js
+++ src/state/reducer.js
@@ -5,32 +5,94 @@ import { handleActions } from 'redux-actions';
  * Summary reducer
  */
 export const summary = handleActions({
-  REQUEST_AGGREGATIONS: state => ({
+  REQUEST_FORM_DIGEST: state => ({
+    count: state.count,
     aggregations: state.aggregations,
     isFetching: true
   }),
 
-  RECEIVE_AGGREGATIONS: (state, action) => ({
+  RECEIVE_FORM_DIGEST: (state, action) => ({
+    count: action.payload.count,
     aggregations: action.payload.aggregations,
     isFetching: false
   })
 }, {
+  count: 0,
   aggregations: null,
   isFetching: false
 });
 
 export const questions = handleActions({
-  REQUEST_QUESTIONS: state => Object.assign({}, state, {
-    isFetching: true
-  }),
+  /**
+   * Populate the questions dictionary, and compute & store a few useful
+   * auxiliary pieces of information in sibling keys. The questions come
+   * back in the same action as the form digest aggregations data and the
+   * list of 10 latest submissions.
+   *
+   * - `store.questions.order` is an ordered array of Question IDs, in the order
+   *    that they occur in the Ask form.
+   * - `store.questions.filters` is an object with `.emoji` and `.topic` keys,
+   *    designating the IDs of the questions to use for the Emoji visualizations
+   *    and Topic visualizations, respectively.
+   * - `store.questions.dictionary` is a dictionary of question objects, keyed
+   *    by Question ID.
+   *
+   * Questions can only be retrieved as a part of the form digest packet, and it
+   * is generally assumed that this reducer will only be run once; for that reason
+   * no `isFetching` key is used here.
+   *
+   * @param {Object} state  The previous state object; this is thrown away entirely
+   * @param {Object} action The RECEIVE_FORM_DIGEST Redux action
+   * @returns {Object} The new state object
+   */
+  RECEIVE_FORM_DIGEST: (state, action) => {
+    const questions = action.payload.questions;
+    const order = Object.keys(questions).sort((a, b) => questions[a].order - questions[b].order);
 
-  RECEIVE_QUESTIONS: (state, action) => ({
-    dictionary: action.payload,
-    isFetching: false
-  })
+    const isGroupingQuestion = question => question.group_by && question.type === 'MultipleChoice';
+
+    // Emoji question is assumed to be the first grouping multiple choice
+    // question with only short text responses
+    const emojiQuestionId = order.find((qId) => {
+      const question = questions[qId];
+
+      if (!isGroupingQuestion(question)) {
+        return false;
+      }
+
+      // Check to see if this question's answers are all short (therefore likely to be emoji);
+      // No undef check is needed on question.options because all MC ?'s have .options
+      return question.options
+        .reduce((noLongResponses, option) => noLongResponses && option.value.length <= 4, true);
+    });
+
+    // Topic grouping question is first non-emoji multiple-choice question
+    const topicQuestionId = order.find((qId) => {
+      if (qId === emojiQuestionId) {
+        return false;
+      }
+
+      const question = questions[qId];
+
+      return isGroupingQuestion(question);
+    });
+
+    return {
+      dictionary: questions,
+      filters: {
+        emoji: emojiQuestionId,
+        topic: topicQuestionId
+      },
+      order
+    };
+  }
 }, {
   dictionary: {},
-  isFetching: false
+  filters: {
+    emoji: null,
+    topic: null
+  },
+  order: []
 });
 
 /**
@@ -84,7 +146,7 @@ export const responses = handleActions({
   }),
 
   // Does not currently impact the isFetching state
-  RECEIVE_AGGREGATIONS: (state, action) => ({
+  RECEIVE_FORM_DIGEST: (state, action) => ({
     dictionary: action.payload.submissions
       .reduce((carry, response) => Object.assign({}, carry, {
         [response.id]: response
