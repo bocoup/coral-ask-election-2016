f08f1d7174ffd7b0bc41ab20b3a2c7e5569c84ee
K. Adam White <kadamwhite@users.noreply.github.com>
2016-10-30 23:26:04 -0400

    Remove outdated generated JSON, and outdated generator code

--- _data-generator/generate-questions-json.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-const outputPath = path.relative(__dirname, '../public/data/questions.json');
-const questions = require('./questions');
-
-fs.writeFileSync(outputPath, JSON.stringify({
-  questions
-}));
